0f64cf7c66cae9a0c274377b37a2aad2650a6f74
Michael Galloway <mxg1410@qsl.com.au>
2026-03-16 13:18:00 +1000

    Update alias

--- flake.nix
+++ flake.nix
@@ -38,6 +38,7 @@
           source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
           eval "$(zoxide init zsh)"
           eval "$(direnv hook zsh)"
+          alias cd=z
           ZSHEOF
 
                     export SHELL="${pkgs.zsh}/bin/zsh"
